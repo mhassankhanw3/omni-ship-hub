2eeb7789e2c7e8c96e9d64a67449166cd4d17b60
mhassankhanw3 <mhassankhanw3@gmail.com>
2024-02-05 12:57:25 +0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
